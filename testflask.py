ec4b262c7713d93c554935fff82e45cba7388623
GradientDescent <39660511+jullmg@users.noreply.github.com>
2022-07-23 20:16:06 -0400

    Update testflask.py

--- testflask.py
+++ testflask.py
@@ -1,9 +1,7 @@
 from flask import Flask
 
-print("Hello world from Python script!")
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>I'm learning Openshift and I like it!</p>"
+    return "<p>Hello world of Openshift!</p>"
